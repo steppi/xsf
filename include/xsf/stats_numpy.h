6214fa4e7e0d26575348321f050a0b782f040c71
steppi <1953382+steppi@users.noreply.github.com>
2026-04-10 13:48:20 -0400

    Try to fix bugs

--- include/xsf/stats_numpy.h
+++ include/xsf/stats_numpy.h
@@ -24,7 +24,7 @@ namespace numpy {
     }
 
     template <typename KMat, typename PMat, typename OutputMat>
-    inline void poisson_binom_pmf(KMat k, PMat p, OutMat out) {
+    inline void poisson_binom_pmf(KMat k, PMat p, OutputMat out) {
         using T = typename OuputMat::value_type;
         auto n = p.extent(0);
         auto k_size = k.extent(0);
@@ -35,14 +35,14 @@ namespace numpy {
         }
         std::vector<T> pmf(n + 1);
         auto pmf_view = detail::make_mdspan(pmf);
-        xsf::poisson_binom_pmf_all(p, pmf_view)
+        xsf::poisson_binom_pmf_all(p, pmf_view);
         for (ptrdiff_t i = 0;  i < k.extent(0); i++) {
             out(i) = xsf::take_from_pmf(pmf_view, static_cast<long long int>(k(i)));
         }
     }
 
     template <typename KMat, typename PMat, typename OutputMat>
-    inline void poisson_binom_cdf(KMat k, PMat p, OutMat out) {
+    inline void poisson_binom_cdf(KMat k, PMat p, OutputMat out) {
         using T = typename OuputMat::value_type;
         auto n = p.extent(0);
         auto k_size = k.extent(0);
@@ -53,7 +53,7 @@ namespace numpy {
         }
         std::vector<T> cdf(n + 1);
         auto cdf_view = detail::make_mdspan(cdf);
-        xsf::poisson_binom_cdf_all(p, cdf_view)
+        xsf::poisson_binom_cdf_all(p, cdf_view);
         for (ptrdiff_t i = 0;  i < k.extent(0); i++) {
             out(i) = xsf::take_from_discrete_cdff(cdf_view, static_cast<long long int>(k(i)));
         }
